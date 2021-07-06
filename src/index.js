2e6fff45d2b0ae7118e70ec2f23d083aa83ff377
Nikolay Lapshin <nlapshin1989@gmail.com>
2021-07-06 10:13:06 +0300

    Revert "Update package"
    This reverts commit 78642b19889b6a677e1e85a5de42c2cb383ed1d4.

--- src/index.js
+++ src/index.js
@@ -1,119 +1,86 @@
+const { Cluster } = require('puppeteer-cluster');
 const { forEach } = require('p-iteration');
 
-const TaskBus = require('async-task-queue-runner');
-const puppeteer = require('puppeteer');
-const got = require('got');
-const { HttpsProxyAgent } = require('hpagent');
-const cheerio = require('cheerio');
-const delay = require('delay');
-
 const Links = require('./links');
-const Pptr = require('./pptr');
 
-module.exports = async function run(sites, options) {
-	const taskBus = new TaskBus([], options.maxConcurrency || 1, options.retryLimit || 0);
-	const decorators = options.decorators;
-	const proxy = options.proxy;
+module.exports = async function run(sites = [], options = {}, decorators = {}) {
+	if (!options.concurrency) {
+		options.concurrency = Cluster.CONCURRENCY_CONTEXT;
+	}
 
-	const context = { taskBus, decorators, proxy, output: {} };
+	const cluster = await Cluster.launch(options);
+	const siteMap = {};
 
-	sites.forEach(site => {
-		taskBus.add(task.bind(null, site, context));
+	await cluster.task(task);
+
+	await forEach(sites, async (site) => {
+		siteMap[site] = await cluster.execute({ url: site, decorators });
 	});
 
-	await taskBus.run();
+	await cluster.idle();
+	await cluster.close();
 
-	return context.output;
-
-	async function task(baseUrl, context) {
-		const { decorators = {} } = context;
-
-		const pptr = new Pptr(options.puppeteer);
-
-		const links = new Links(baseUrl);
-		links.register(baseUrl);
-
-		await pptr.start();
-
-		for (;;) {
-			const nextUrl = links.getFirstUncheckedUrl();
-
-			if (nextUrl === null) {
-				break;
-			}
-
-			try {
-				if (decorators.beforeEach) {
-					await decorators.beforeEach.call(decorators, context, nextUrl, baseUrl);
-				}
-
-				await parseHandlerPuppeteer(nextUrl, links, pptr, context);
-				// await parseHandler(nextUrl, links, context);
-
-				throw 'test';
-
-				if (decorators.afterEach) {
-					await decorators.afterEach.call(decorators, context, nextUrl, baseUrl);
-				}
-			} catch(error) {
-				console.error(error);
-
-				throw error;
-			}
-
-			const checkedCount = links.getCheckedLinks().length;
-			const allCount = links.getInternalLinks().length;
-
-			console.log(`${baseUrl}: ${checkedCount}/${allCount}`);
-		}
-
-		context.output[baseUrl] = links.getInternalLinks();
-
-		await pptr.stop();
-	}
-
-	async function parseHandler(url, links, context) {
-		const response = await makeRequest(url, context.proxy);
-		const $ = cheerio.load(response.body);
-
-		const linkObjects = $('a');
-		const hrefs = [];
-
-		linkObjects.each((index, element) => {
-			const href = links.addPrefixIfNeed($(element).attr('href'));
-
-			hrefs.push(href);
-		});
-
-		links.setChecked(url);
-		links.collect(hrefs);
-	}
-
-	async function parseHandlerPuppeteer(url, links, pptr) {
-		const hrefs = await pptr.hrefs(url);
-
-		links.setChecked(url);
-		links.collect(hrefs);
-	}
-
-	function makeRequest(url, proxy) {
-		const options = {};
-
-		if (proxy) {
-			options.agent = {
-				https: new HttpsProxyAgent({
-					keepAlive: true,
-					keepAliveMsecs: 1000,
-					maxSockets: 256,
-					maxFreeSockets: 256,
-					scheduling: 'lifo',
-					proxy: 'https://localhost:8080'
-				})
-			};
-		}
-
-		console.log(url);
-
-		return got(url, options);
-	}
+	return siteMap;
 };
+
+async function task({ page, data })  {
+	const { url: baseUrl, decorators } = data;
+
+	if (decorators.skip) {
+		const skip = await decorators.skip.call(decorators, page, baseUrl);
+		
+		if (skip === true) {
+			return Promise.resolve([]);
+		}
+	}
+
+	if (decorators.before) {
+		await decorators.before.call(decorators, page, baseUrl);
+	}
+
+	const links = new Links(baseUrl);
+
+	links.register(baseUrl);
+
+	for (;;) {
+		const nextUrl = links.getFirstUncheckedUrl();
+
+		if (nextUrl === null) {
+			break;
+		}
+
+		try {
+			if (decorators.beforeEach) {
+				await decorators.beforeEach.call(decorators, page, nextUrl, baseUrl);
+			}
+
+			await findAllLinks(page, nextUrl, links);
+
+			if (decorators.afterEach) {
+				await decorators.afterEach.call(decorators, page, nextUrl, baseUrl);
+			}
+		} catch(error) {
+			console.error(error);
+		}
+
+		const checkedCount = links.getCheckedLinks().length;
+		const allCount = links.getInternalLinks().length;
+
+		console.log(`${baseUrl}: ${checkedCount}/${allCount}`);
+	}
+
+	if (decorators.after) {
+		await decorators.after.call(decorators, page, baseUrl);
+	}
+
+	return links.getInternalLinks();
+}
+
+async function findAllLinks(page, url, links) {
+	await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+	const hrefs = await page.$$eval('a', as => as.map(a => a.href));
+
+	links.setChecked(url);
+	links.collect(hrefs);
+}
